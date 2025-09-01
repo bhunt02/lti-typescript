aa8a153bdfdafe9c5479004c1026d470b2340e1e
bhunt02 <bridgette.hunt02@outlook.com>
2025-08-31 21:49:17 -0700

    minor changes

--- src/provider/services/dynamic_registration.ts
+++ src/provider/services/dynamic_registration.ts
@@ -109,7 +109,7 @@ export class DynamicRegistrationService {
         throw err;
       });
 
-    Debug.log(this, 'OpenID Configuration: ', configuration);
+    Debug.log(this, 'OpenID Configuration: ', JSON.stringify(configuration));
     Debug.log(
       this,
       'Attempting to register Platform with issuer: ',
